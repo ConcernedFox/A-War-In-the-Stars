d0d8e336cc7c50bc6bfbd355491f20b52652b5a9
ConcernedFox <vihaan.gururani@gmail.com>
2025-11-09 09:56:49 -0500

    Add files via upload

--- A War in the Stars.py
+++ A War in the Stars.py
@@ -3,7 +3,7 @@ pygame.init()
 
 WIDTH = 800
 HEIGHT = 600
-Screen = pygame.display.set_mode((WIDTH,HEIGHT))
+Screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
 E = pygame.image.load("A.png")
 C = pygame.transform.rotate(E, -90)
@@ -30,7 +30,9 @@ def draw_screen(R, L, A, B):
     Screen.blit(B_health, (550,60))
 
 R = pygame.Rect(100, 500, 39, 39)
-L = pygame.Rect(650, 500, 39, 39)
+B = pygame.Rect(650, 500, 39, 39)
+Rshoot = pygame.USEREVENT + 1
+Bshoot = pygame.USEREVENT + 2
 RH = 10
 LH = 10
 def R_move(key_pressed, Rect):
@@ -55,9 +57,13 @@ def L_move(key_pressed, Rect):
 
 R_BULLETS = []
 L_BULLETS = []
-
 Max_Bullets = 6
 
+def R_and_B(R, B, R_BULLETS, L_BULLETS):
+    for r in R_BULLETS:
+        r.x += 10
+    for l in L_BULLETS:
+        l.x -= 10
 
 while True:
     for p in pygame.event.get():
@@ -70,10 +76,11 @@ while True:
                     R_BULLETS.append(A)
             if p.key == pygame.K_m:
                 if len(L_BULLETS) < Max_Bullets:
-                    B = pygame.Rect(L.x, L.y + 20, 10, 5)
-                    L_BULLETS.append(B)
+                    G = pygame.Rect(B.x, B.y + 20, 10, 5)
+                    L_BULLETS.append(G)
     K = pygame.key.get_pressed()
     R_move(K, R)
-    L_move(K, L)
-    draw_screen(R, L, R_BULLETS, L_BULLETS)
+    L_move(K, B)
+    draw_screen(R, B, R_BULLETS, L_BULLETS)
+    R_and_B(R, B, R_BULLETS, L_BULLETS)
     pygame.display.update()
